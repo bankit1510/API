9a5c4a2f636f9689e364b699b29103e889ff4549
Ankitsingh Bisht <ankitsingh.cs18018@mmcc.edu.in>
2022-12-13 17:19:30 +0530

    Minor changes

--- Routes/users.js
+++ Routes/users.js
@@ -1,28 +1,18 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
-import fs from "fs";
+import fs, { read } from "fs";
 import validator from "validator";
 
 const router = express.Router();
 
-let users = [];
-// Reading the file in global scope
-fs.readFile(".././data.json", (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    users = JSON.parse(data);
-  }
-});
-
-//Getting the data from file
-
 router.get("/", (req, res) => {
-  res.send(users);
+  res.send(readFile());
 });
 // Adding data to the file
 router.post("/", (req, res) => {
   const user = req.body;
+  req.body.age = parseInt(req.body.age);
+  console.log(req.body);
   if (
     !validator.isAlpha(user.fName) ||
     !validator.isAlpha(user.lName) ||
@@ -34,27 +24,29 @@ router.post("/", (req, res) => {
       ...user,
       id: uuidv4(),
     };
-    res.send(`User Data Pushed with id ${userWithID.id}`);
+    let users = readFile();
     users.push(userWithID);
+    let json = JSON.stringify(users);
+    fs.writeFile(".././data.json", json, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("\nFile Contents pushed");
+        res.send(`User Data Pushed with id ${userWithID.id}`);
+      }
+    });
   }
-  let json = JSON.stringify(users);
-
-  fs.writeFile(".././data.json", json, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("\nFile Contents pushed");
-    }
-  });
 });
 // to get the details about the specific  id
 router.get("/:id", (req, res) => {
+  const users = readFile();
   const { id } = req.params;
   const foundUser = users.find((user) => user.id === id);
   res.send(foundUser);
 });
 // To delete the user
 router.delete("/:id", (req, res) => {
+  var users = readFile();
   const { id } = req.params;
   users = users.filter((user) => user.id !== id);
   let json = JSON.stringify(users);
@@ -62,7 +54,7 @@ router.delete("/:id", (req, res) => {
     if (err) {
       console.log(err);
     } else {
-      console.log("\nFile Contents pushed using patch");
+      console.log("\nFile Contents deleted");
     }
   });
   res.send(`User with id ${id} deleted`);
@@ -71,6 +63,7 @@ router.delete("/:id", (req, res) => {
 //Patch is for updating directly if doesn't exist then return error
 
 router.patch("/:id", (req, res) => {
+  const users = readFile();
   const { id } = req.params;
   const { fName, lName, age } = req.body;
   const user = users.find((user) => user.id === id);
@@ -85,6 +78,13 @@ router.patch("/:id", (req, res) => {
       console.log("\nFile Contents pushed using patch");
     }
   });
+
   res.send(`user with ${id} has been updated`);
 });
+
+const readFile = () => {
+  const jsonData = fs.readFileSync(".././data.json");
+  return JSON.parse(jsonData);
+};
+
 export default router;
--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-import express from 'express'
-import bodyParser from 'body-parser';
-import router from './Routes/users.js';
+import express from "express";
+import bodyParser from "body-parser";
+import router from "./Routes/users.js";
+import cors from "cors";
 
 const app = express();
 
-const PORT=3000;
-
+const PORT = 3000;
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.urlencoded());
 
-app.use('/users',router);
+app.use("/users", router);
 
-app.get('/',(req,res)=>{
-    res.send('Hello from homepage')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from homepage");
+});
 
-app.listen(PORT,()=>{
-    console.log("Server is Running on http://localhost:3000/");
-})
+app.listen(PORT, () => {
+  console.log("Server is Running on http://localhost:3000/");
+});
